8f4debb3083a3ab2ddc3b3df50ee73409a49c30e
yuchuangu85 <yuchuangu85@gmail.com>
2026-08-11 10:45:33 +0800

    chore: bump version to 0.4.5

--- desktop-viewer/build.gradle.kts
+++ desktop-viewer/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     alias(libs.plugins.kotlin.serialization) apply false
 }
 
-val defaultAppVersion = "0.4.4"
+val defaultAppVersion = "0.4.5"
 val appVersion = providers.gradleProperty("appVersion").getOrElse(defaultAppVersion)
 
 allprojects {
